e6d92ff1704a9592cd42b0543aa563427e5bf28e
fatigzzr <fatima.gonzalezr@udem.edu>
2023-04-09 23:23:06 -0600

    project01

---
+++ 1. WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Fátima González on 09/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
